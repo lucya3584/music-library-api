117a016673983820d1624d1e8226666849029f2b
lucya3584 <lucya3584@yahoo.co.uk>
2020-01-31 17:26:14 +0000

    artists tests passing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const artistControllers = require('./controllers/artists.js');
 
 const app = express();
+app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
-});
+app.post('/artists', artistControllers.create);
 
 module.exports = app;
