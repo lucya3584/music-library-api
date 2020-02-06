b7c6a918473856a8b7ae40dc338604d629fcbeb9
lucya3584 <lucya3584@yahoo.co.uk>
2020-02-06 20:59:19 +0000

    updated and deleted artists

--- src/app.js
+++ src/app.js
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.post('/artists', artistControllers.create);
+app.get('/artists', artistControllers.list);
+app.get('/artists/:id', artistControllers.find);
+app.patch('/artists/:id', artistControllers.patch);
+app.delete('/artists/:artistId', artistControllers.delete);
 
 module.exports = app;
--- src/controllers/artists.js
+++ src/controllers/artists.js
@@ -15,3 +15,25 @@ exports.list = (req, res) => {
     res.status(200).send(artists);
   });
 };
+exports.find = (req, res) => {
+  Artist.findOne({ _id: req.params.id }, (err, artist) => {
+    res.status(200).json(artist);
+  });
+};
+exports.patch = (req, res) => {
+  Artist.findOne({ _id: req.params.id }, (_, artist) => {
+    artist.set(req.body);
+    artist.save().then(updatedArtist => {
+      res.status(200).json(updatedArtist);
+    });
+  });
+};
+exports.delete = (req, res) => {
+  Artist.findOneAndDelete({ _id: req.params.artistId }, (err, artist) => {
+    if (!artist) {
+      res.status(404).json({ error: 'The artist could not be found.' });
+    } else {
+      res.status(204).json(artist);
+    }
+  });
+};
